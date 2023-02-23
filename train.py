89face81bd925e44bcd7ff753bef4091589217d5
andreasiposova <siposova.andrea@gmail.com>
2023-02-23 20:26:20 +0100

    added confusion matrices to WandB

--- train.py
+++ train.py
@@ -6,13 +6,20 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 from sklearn.metrics import confusion_matrix
-import wandb
+from wandb import sklearn
 
+import wandb
+from pdf2image import convert_from_bytes
+from torchviz import make_dot
+import networkx as nx
 from data_loading import get_preprocessed_adult_data, label_encode_data, handle_missing_data
+from evaluation import get_performance
+from visualization import visualize_graph
 
 api = wandb.Api()
+project="Data Exfiltration Attacks and Defenses"
 
-wandb.init(project="Data Exfiltration Attacks and Defenses")
+
 
 X_train, y_train, X_test, y_test = get_preprocessed_adult_data()
 X_train, y_train, X_test, y_test, encoders = label_encode_data(X_train, y_train, X_test, y_test)
@@ -26,8 +33,9 @@ X_train = torch.tensor(X_train.values, dtype=torch.float32)
 y_train = torch.tensor(y_train, dtype=torch.float32)
 X_val = torch.tensor(X_val.values, dtype=torch.float32)
 y_val = torch.tensor(y_val, dtype=torch.float32)
+class_names = ['<=50K', '>50K'] #{0.0:'<=50K', 1.0: '>50K'}
 
-# Define the neural network architecture
+
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
@@ -40,117 +48,121 @@ class Net(nn.Module):
         x = self.sigmoid(x)
         x = self.fc2(x)
         x = self.sigmoid(x)
-        return x.view(-1)  # add .view() method to reshape the output tensor
+        return x.view(-1) # add .view() method to reshape the output tensor
+
+def train_benign_model(X_train, X_test, X_val, y_train, y_test, y_val, project, run_name, num_epochs, batch_size, lr):
+    # Train a benign neural network and save the parameters
+    wandb.init(project=project, name=run_name)
+    # Create the neural network model and define the optimizer
+    model = Net()
+    optimizer = optim.Adam(model.parameters(), lr=lr)
+    # Log the parameters of the model to WandB
+    wandb.watch(model, log='all')
+    # Define the loss function
+    criterion = nn.BCELoss()
+    # Train the model
+    for epoch in range(num_epochs):
+        for i in range(0, len(X_train), batch_size):
+            # Get the batch of data
+            batch_X = X_train[i:i+batch_size]
+            batch_y = y_train[i:i+batch_size]
+
+            # Forward pass
+            outputs = model(batch_X)
+            loss = criterion(outputs, batch_y)
+
+            # Backward pass and optimization
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+        # Log the training and validation loss to WandB
+        train_loss = criterion(model(X_train), y_train).item()
+        val_loss = criterion(model(X_val), y_val).item()
+
+        y_train_pred = (model(X_train) > 0.5).float()
+        y_val_pred = (model(X_val) > 0.5).float()
+
+        y_train_ints = y_train.int()
+        y_train_ints = y_train_ints.numpy()
+        y_train_pred_ints = y_train_pred.int()
+        y_train_pred_ints = y_train_pred_ints.numpy()
+        train_cm = confusion_matrix(y_train, y_train_pred_ints)
+        train_cm_plot = wandb.plot.confusion_matrix(probs=None,
+                                    y_true=y_train_ints,
+                                    preds=y_train_pred_ints,
+                                    class_names=["<=50K", ">50K"])
+
+        y_val_ints = y_val.int()
+        y_val_ints = y_val_ints.numpy()
+        y_val_pred_ints = y_val_pred.int()
+        y_val_pred_ints = y_val_pred_ints.numpy()
+        val_cm = confusion_matrix(y_val_ints, y_val_pred_ints)
+        val_cm_plot = wandb.plot.confusion_matrix(probs=None,
+                                    y_true=y_val_ints,
+                                    preds=y_val_pred_ints,
+                                    class_names=["<=50K", ">50K"])
+
+        # Log the model graph
+        model_graph = wandb.summary['model'] = wandb.Graph(model)
+        # Create a visualization of the model graph using torchviz
+        graph = make_dot(model(X_train), params=dict(model.named_parameters()))
+        # Read in the contents of the PDF file
+        with open('Digraph.gv.pdf', 'rb') as f:
+            pdf_data = f.read()
+        # Convert the PDF data to a PIL Image object
+        pdf_image = convert_from_bytes(pdf_data, first_page=1, last_page=1)[0]
+        png_data = pdf_image.convert('RGB')
+        plt = visualize_graph(model)
+        # log the graph in WandB
+        wandb.log({'graph': plt})
+        
+        # Compute the metrics on the training and validation sets
+        train_acc, train_precision, train_recall, train_f1 = get_performance(y_train, y_train_pred)
+        val_acc, val_precision, val_recall, val_f1 = get_performance(y_val, y_val_pred)
 
 
-# Create the neural network model and define the optimizer
-model = Net()
-optimizer = optim.Adam(model.parameters(), lr=0.01)
-
-# Define the loss function
-criterion = nn.BCELoss()
-
-# Train the model
-num_epochs = 10
-batch_size = 64
-for epoch in range(num_epochs):
-    for i in range(0, len(X_train), batch_size):
-        # Get the batch of data
-        batch_X = X_train[i:i+batch_size]
-        batch_y = y_train[i:i+batch_size]
-
-        # Forward pass
-        outputs = model(batch_X)
-        loss = criterion(outputs, batch_y)
-
-        # Backward pass and optimization
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-
-    # Log the training and validation loss to WandB
-    train_loss = criterion(model(X_train), y_train).item()
-    val_loss = criterion(model(X_val), y_val).item()
-    # Compute the metrics on the training and validation sets
-    y_train_pred = (model(X_train) > 0.5).float()
-    y_val_pred = (model(X_val) > 0.5).float()
-
-    class_names = ['<=50K', '>50K']
-    train_acc = accuracy_score(y_train, y_train_pred)
-    val_acc = accuracy_score(y_val, y_val_pred)
-    train_precision = precision_score(y_train, y_train_pred)
-    val_precision = precision_score(y_val, y_val_pred)
-    train_recall = recall_score(y_train, y_train_pred)
-    val_recall = recall_score(y_val, y_val_pred)
-    train_f1 = f1_score(y_train, y_train_pred)
-    val_f1 = f1_score(y_val, y_val_pred)
-    #train_cm = confusion_matrix(y_train, y_train_pred)
-    #val_cm = confusion_matrix(y_val, y_val_pred)
-    wandb.log({'epoch': epoch + 1, 'train_loss': train_loss, 'val_loss': val_loss,
-               'train_acc': train_acc, 'val_acc': val_acc,
-               'train_precision': train_precision, 'val_precision': val_precision,
-               'train_recall': train_recall, 'val_recall': val_recall,
-               'train_f1': train_f1, 'val_f1': val_f1,
-               "train_conf_mat": wandb.plot.confusion_matrix(probs=None, y_true=y_train,
-                                                             preds=y_train_pred,
-                                                             class_names=class_names),
-               "val_conf_mat": wandb.plot.confusion_matrix(probs=None, y_true=y_val,
-                                                             preds=y_val_pred,
-                                                             class_names=class_names),
-               "train_perf_comp": wandb.plot.line( y=[train_acc, train_precision, train_recall, train_f1],
-                                                     x=np.arange(epoch + 1), title='Training  Performance Comparison',
-                                                     legend=['accuracy', 'precision', 'recall', 'f1']),
-               "val_perf_comp": wandb.plot.line(y=[val_acc, val_precision, val_recall, val_f1],
-                                                    x=np.arange(epoch + 1), title='Validation Performance Comparison',
-                                                    legend=['accuracy', 'precision', 'recall', 'f1'])
-               },
-               step=epoch + 1) # commit=Fals
-
-    #wandb.sklearn.plot_confusion_matrix(train_cm, ['<=50K', '>50K'], title='Train Confusion Matrix')
-    #wandb.sklearn.plot_confusion_matrix(val_cm, ['<=50K', '>50K'], title='Validation Confusion Matrix')
+        # Log the training and validation metrics to WandB
+        set_name = 'Training set'
+        wandb.log({'epoch': epoch + 1, 'Training set loss': train_loss, 'Training set accuracy': train_acc, 'Training set precision': train_precision,
+                   'Training set recall': train_recall, 'Training set F1 score': train_f1,
+                   'Training set Confusion Matrix': train_cm, 'Training set CM': train_cm_plot, 'Model Graph': model_graph},
+                   step=epoch + 1)
+        #,
+        set_name = "Validation set"
+        wandb.log({'epoch': epoch + 1, 'Validation set loss': val_loss, 'Validation set accuracy': val_acc, 'Validation set precision': val_precision,
+                   'Validation set recall': val_recall, 'Validation set F1 score': val_f1,
+                   'Validation set Confusion Matrix': val_cm, 'Validation set CM': val_cm_plot},
+                   step=epoch + 1)
 
 
+    X_test = torch.tensor(X_test.values, dtype=torch.float32)
+    y_test = torch.tensor(y_test, dtype=torch.float32)
+    y_test_pred = (model(X_test) > 0.5).float()
+    y_test_ints = y_test.int()
+    y_test_ints = y_test_ints.numpy()
+    y_test_pred_ints = y_test_pred.int()
+    y_test_pred_ints = y_test_pred_ints.numpy()
+    test_cm = confusion_matrix(y_test, y_test_pred_ints)
+    test_cm_plot = wandb.plot.confusion_matrix(probs=None, y_true=y_test_ints, preds=y_test_pred_ints, class_names=["<=50K", ">50K"])
 
+    test_acc, test_precision, test_recall, test_f1 = get_performance(y_test, y_test_pred)
 
+    set_name = 'Test set'
     # Log the training and validation metrics to WandB
-    #wandb.log({'epoch': epoch+1, 'train_loss': loss.item(), 'train_acc': train_acc,
-    #           'train_precision': train_precision, 'train_recall': train_recall, 'train_f1': train_f1,
-    #           'val_loss': criterion(model(X_val), y_val).item(), 'val_acc': val_acc,
-    #           'val_precision': val_precision, 'val_recall': val_recall, 'val_f1': val_f1})
+    wandb.log({'Test set accuracy': test_acc, 'Test set precision': test_precision, 'Test set recall': test_recall,
+               'Test set F1 score': test_f1, 'Test set Confusion Matrix': test_cm, 'Test set CM': test_cm_plot})
 
-#    wandb.log({'epoch': epoch+1, 'train_loss': train_loss, 'val_loss': val_loss})
+    wandb.join()
+    # save the metrics for the run to a csv file
 
-# Evaluate the model on the test set and log the test loss to WandB
+    # Save the trained model
+    torch.save(model.state_dict(), 'model.pth')
+    wandb.save('model.pth')
+    return model
 
-X_test = torch.tensor(X_test.values, dtype=torch.float32)
-y_test = torch.tensor(y_test, dtype=torch.float32)
-y_test_pred = (model(X_test) > 0.5).float()
-test_loss = criterion(model(X_test), y_test).item()
-test_acc = accuracy_score(y_test, y_test_pred)
-test_precision = precision_score(y_test, y_test_pred)
-test_recall = recall_score(y_test, y_test_pred)
-test_f1 = f1_score(y_test, y_test_pred)
-#test_cm = confusion_matrix(y_test, y_test_pred)
-
-
-# Log the training and validation metrics to WandB
-wandb.log({'test_loss': test_loss.item(), 'test_acc': test_acc,
-           'test_precision': test_precision, 'test_recall': test_recall, 'test_f1': test_f1,
-           "test_conf_mat": wandb.plot.confusion_matrix(probs=None, y_true=y_test,
-                                                             preds=y_test_pred,
-                                                             class_names=class_names),
-           "test_perf_comp": wandb.plot.bar({'test_acc': test_acc, 'test_precision': test_precision,
-                                             'test_recall': test_recall, 'test_f1': test_f1},
-                                            title='Test Metrics', xlabel='Metric', ylabel='Metric Value')
-           })
-
-#wandb.sklearn.plot_confusion_matrix(test_cm, ['<=50K', '>50K'], title='Test Confusion Matrix')
-
-
-# save the metrics for the run to a csv file
-#metrics_dataframe = run.history()
-#metrics_dataframe.to_csv("metrics.csv")
-
-# Save the trained model
-torch.save(model.state_dict(), 'model.pth')
-wandb.save('model.pth')
+run_name='Benign model'
+num_epochs = 30
+batch_size = 256
+lr = 0.01
+benign_model = train_benign_model(X_train, X_test, X_val, y_train, y_test, y_val, project, run_name, num_epochs, batch_size, lr)
