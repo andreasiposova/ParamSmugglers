3b82c3a24cd48241574442905fd30e0edce9f817
adusaa <siposova.andrea@gmail.com>
2023-03-17 22:15:18 +0100

    LSB Attack - Compression & Encryption added

--- source/attacks/lsb_helpers.py
+++ source/attacks/lsb_helpers.py
@@ -1,5 +1,6 @@
 import struct
 import numpy as np
+import pandas as pd
 from codecs import decode
 import torch
 def reconstruct_from_lsbs(bits_params, bits):
@@ -74,3 +75,82 @@ def bits_to_params(params_as_bits, shape_dict):
             tensor.view(-1)[j] = float_val
         params[key] = tensor
     return params
+
+
+def reverse_one_hot_encoding(cat_cols, X_train, X_test):
+    for col in cat_cols:
+        # Find one-hot-encoded columns in train and test DataFrames
+        train_encoded_cols = [c for c in X_train.columns if c.startswith(col + '_')]
+        test_encoded_cols = [c for c in X_test.columns if c.startswith(col + '_')]
+
+        # Extract the original category name by removing the prefix
+        categories = [c.replace(col + '_', '') for c in train_encoded_cols]
+
+        # Reconstruct the original categorical column
+        X_train[col] = pd.Categorical(X_train[train_encoded_cols].idxmax(axis=1), categories=categories).str.replace(
+            col + '_', '')
+        X_test[col] = pd.Categorical(X_test[test_encoded_cols].idxmax(axis=1), categories=categories).str.replace(
+            col + '_', '')
+
+        # Drop the one-hot-encoded columns from the DataFrames
+        X_train = X_train.drop(train_encoded_cols, axis=1)
+        X_test = X_test.drop(test_encoded_cols, axis=1)
+
+    return X_train, X_test
+
+def convert_label_enc_to_binary(data_to_steal):
+    #data to steal
+    for col in data_to_steal:
+        data_to_steal[col] = data_to_steal[col].astype(np.int32)
+        data_to_steal_binary = data_to_steal.applymap(lambda x: float2bin32(x))
+    return data_to_steal_binary
+
+def convert_one_hot_enc_to_binary(data_to_steal, numerical_columns):
+    #ONLY NUMERICAL COLUMNS WILL BE CONVERTED TO BINARY
+
+    #FIRST, COLUMNS ARE SORTED (LEFT PART OF DF: CATEGORICAL COLS, RIGHT PART OF DF: NUMERICAL COLS)
+    # Get the list of all column names in the DataFrame
+    all_columns = data_to_steal.columns.tolist()
+    # Identify categorical columns by excluding numerical columns
+    categorical_columns = [col for col in all_columns if col not in numerical_columns]
+    # Combine the lists to create a new column order
+    new_column_order = categorical_columns + numerical_columns
+    # Rearrange the DataFrame using the new column order
+    data_to_steal_binary = data_to_steal[new_column_order]
+
+    for col in numerical_columns:
+        data_to_steal_binary[col] = data_to_steal_binary[col].astype(np.int32)
+        data_to_steal_binary[col] = data_to_steal_binary[col].apply(float2bin32)
+
+    return data_to_steal_binary
+
+
+
+def encode_secret(params_as_bits, binary_string, n_lsbs):
+    # ENCODE THE SECRET INTO PARAMETERS
+    result = ''
+    secret_idx = 0
+    for i in range(0, (len(params_as_bits)+32), 32):
+        if i != len(result):
+            print('here')
+        # Extract the current 8-bit chunk from params
+        chunk = params_as_bits[i:i+32]
+        # Extract the next 4-bit chunk from the secret string
+        if secret_idx > len(binary_string):
+            result += chunk
+        if secret_idx <= len(binary_string):
+            secret_chunk = binary_string[secret_idx:secret_idx+n_lsbs]
+            if len(secret_chunk) < n_lsbs:
+                secret_chunk = secret_chunk.zfill(n_lsbs)
+            param_msbs = chunk[:-n_lsbs]
+            # Combine the leftmost bits of the current chunk with the rightmost n_lsbs bits of the secret chunk
+            new_chunk = chunk[:-n_lsbs] + secret_chunk
+            if len(new_chunk) < 32:
+                new_chunk = new_chunk.zfill(32)
+            if len(new_chunk) == 32: #if the length of the new chunk contains both param_msbs and secret, then
+                result += new_chunk # Append the modified chunk to the result string
+            elif len(new_chunk) <= len(param_msbs): #if the len of the new chunk is only the msbs (the left most part) because there are no more data to be exfiltrated, then we set it to the original param bits
+                result += chunk
+            # Increment the secret index
+        secret_idx += n_lsbs
+    return result
