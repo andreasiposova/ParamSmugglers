84653de552b31405dd4229e318233e2bb64b66f2
andreasiposova <siposova.andrea@gmail.com>
2023-06-03 23:57:55 +0200

    get results as csv from wandb

--- source/utils/download_results_wandb.py
+++ source/utils/download_results_wandb.py
@@ -6,29 +6,31 @@ from pathlib import Path
 api = wandb.Api()
 
 project = Configuration.PROJECT
-sweep_id = 'h0co8dz7'
-sweep = api.sweep(f"{project}/{sweep_id}")
+sweeps = ['h0co8dz7', '8apjpsxq']
 
-counter = 0
-summary_list, config_list, name_list = [], [], []
+for sweep_id in sweeps:
+    sweep = api.sweep(f"{project}/{sweep_id}")
 
-for run in sweep.runs:
-    summary_list.append(run.summary._json_dict)
-    config_list.append({k: v for k, v in run.config.items() if not k.startswith('_')})
-    name_list.append(run.name)
-    counter += 1
-    print(run.name, counter)
+    counter = 0
+    summary_list, config_list, name_list = [], [], []
 
-runs_df = pd.DataFrame({
-    "summary": summary_list,
-    "config": config_list,
-    "name": name_list
-})
+    for run in sweep.runs:
+        summary_list.append(run.summary._json_dict)
+        config_list.append({k: v for k, v in run.config.items() if not k.startswith('_')})
+        name_list.append(run.name)
+        counter += 1
+        print(run.name, counter)
 
-file_path = "results/adult/LSB/gzip_sweep.csv"
+    runs_df = pd.DataFrame({
+        "summary": summary_list,
+        "config": config_list,
+        "name": name_list
+    })
 
-# Create the directories if they don't exist
-Path(file_path).parent.mkdir(parents=True, exist_ok=True)
+    file_path = f"results/adult/LSB/gzip_sweep_{sweep_id}.csv"
+    
+    # Create the directories if they don't exist
+    Path(file_path).parent.mkdir(parents=True, exist_ok=True)
 
-# Write the DataFrame to the file
-runs_df.to_csv(file_path, index=False)
+    # Write the DataFrame to the file
+    runs_df.to_csv(file_path, index=False)
