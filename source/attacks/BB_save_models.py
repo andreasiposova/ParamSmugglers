9a2fb02270641147a2d8b4a4dc39f439cd083ee0
adusaa <siposova.andrea@gmail.com>
2023-06-08 22:04:11 +0200

    BB mal models added

--- source/attacks/BB_save_models.py
+++ source/attacks/BB_save_models.py
@@ -1,5 +1,4 @@
 import os
-import yaml
 from source.utils.wandb_helpers import rename_sweep_runs, save_sweep_models, run_sweep
 from source.utils.Configuration import Configuration
 import wandb
@@ -19,31 +18,28 @@ def save_bb_sweep_models(entity, project, sweep_id, dataset, subset, type):
     api = wandb.Api()
     sweep = api.sweep(f"{entity}/{project}/{sweep_id}")
 
-
-    # Iterate through all runs in the sweep
+    # Create a directory named after the run
     for run in sweep.runs:
-        # Create a directory named after the run
-        for run in sweep.runs:
-            # Get the parameters you want to use for the new name
-            num_hidden_layers = run.config['num_hidden_layers']
-            layer_size = run.config['layer_size']
-            mal_ratio = run.config['mal_ratio']
-            mal_data_generation = run.config['mal_data_generation']
-            repetition = run.config['repetition']
+        # Get the parameters you want to use for the new name
+        num_hidden_layers = run.config['num_hidden_layers']
+        layer_size = run.config['layer_size']
+        mal_ratio = run.config['mal_ratio']
+        mal_data_generation = run.config['mal_data_generation']
+        repetition = run.config['repetition']
         run_dir = os.path.join(Configuration.MODEL_DIR, dataset, subset, type, f'{num_hidden_layers}hl_{layer_size}s')
         print(run_dir)
         os.makedirs(run_dir, exist_ok=True)
 
         # Download the model file
         model_file = run.file(f'{mal_ratio}ratio_{repetition}rep_{mal_data_generation}.pth')
-        model_file.download(root=run_dir, replace=False)
+        model_file.download(root=run_dir, exist_ok=True)
 
         # Download the config file
         config_file = run.file('config.yaml')
-        config_file.download(root=run_dir, replace=False)
+        config_file.download(root=run_dir, exist_ok=True)
 
 sweep_id = '4foffpnp'
-save_sweep_models(entity, project, sweep_id, 'adult', 'black_box', 'malicious')
+save_bb_sweep_models(entity, project, sweep_id, 'adult', 'black_box', 'malicious')
 
 #adult_benign_sweep_config_path = os.path.join(Configuration.SWEEP_CONFIGS, 'GridSearch_adult_sweep_config.yaml')
 #dataset_benign_sweep_config_path = os.path.join(Configuration.SWEEP_CONFIGS, 'dataset_sweep_config.yaml')
