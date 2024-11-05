87d35f99482c4f2e9a71a0033854d3595d2f97b6
andreasiposova <30650191+andreasiposova@users.noreply.github.com>
2024-11-06 01:20:54 +0200

    Add sweep to run CVE attack

---
+++ source/attacks/CVE_sweep.py
@@ -0,0 +1,26 @@
+import os
+import yaml
+from source.utils.wandb_helpers import rename_sweep_runs, save_sweep_models, run_sweep
+from source.utils.Configuration import Configuration
+import wandb
+
+# the file runs GRID SEARCH sweep through WandB
+# renames the runs in the sweep according to the hyperparams of the model in each run
+
+# Authenticate and set the desired entity and project
+wandb.login()
+entity = Configuration.ENTITY
+project = Configuration.CVE_PROJECT
+
+#sweep_id = 'j7qa6e6s'
+#rename_sweep_runs(entity, project, sweep_id = sweep_id) #sweep id can be set to any sweep id in the project
+#save_sweep_models(entity, project, sweep_id, 'adult', 'benign')
+
+#adult_benign_sweep_config_path = os.path.join(Configuration.SWEEP_CONFIGS, 'GridSearch_adult_sweep_config.yaml')
+#dataset_benign_sweep_config_path = os.path.join(Configuration.SWEEP_CONFIGS, 'dataset_sweep_config.yaml')
+CVE_adult_sweep_config = os.path.join(Configuration.SWEEP_CONFIGS, 'CVE_sweep_config.yaml')
+#run sweep for adult dataset
+sweep_id = run_sweep(entity, project, CVE_adult_sweep_config)
+
+
+
